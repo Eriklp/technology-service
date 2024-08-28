55fdf1319d73d7285d9f71811d153cb736806afc
eriklp <lopez.erik@correounivalle.edu.co>
2024-08-27 19:40:09 -0500

    feature: adds openApi documentation

--- src/main/java/com/example/technology/api/TechnologyController.java
+++ src/main/java/com/example/technology/api/TechnologyController.java
@@ -2,6 +2,7 @@ package com.example.technology.api;
 
 import com.example.technology.application.TechnologyService;
 import com.example.technology.domain.Technology;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class TechnologyController {
     public TechnologyController(TechnologyService technologyService) {
         this.technologyService = technologyService;
     }
-
+    @Operation(summary = "Get all technologies", description = "Retrieves a paginated list of technologies")
     @GetMapping
     public ResponseEntity<List<Technology>> getAllTechnologies(
             @RequestParam(defaultValue = "ASC") String sortDirection,
@@ -27,14 +28,14 @@ public class TechnologyController {
         List<Technology> technologies = technologyService.getAllTechnologies(sortDirection, page, size);
         return new ResponseEntity<>(technologies, HttpStatus.OK);
     }
-
+    @Operation(summary = "Get technology by id", description = "Retrieves a technology by id")
     @GetMapping("/{id}")
     public ResponseEntity<Technology> getTechnologyById(@PathVariable Long id) {
         Technology technology = technologyService.getTechnologyById(id);
         return technology != null ? new ResponseEntity<>(technology, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
+    @Operation(summary = "Post create technology", description = "Create new technology")
     @PostMapping
     public ResponseEntity<Technology> createTechnology(@Valid @RequestBody Technology technology) {
         try {
@@ -44,7 +45,7 @@ public class TechnologyController {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
-
+    @Operation(summary = "Delete technology by Id", description = "Delete technology by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTechnology(@PathVariable Long id) {
         technologyService.deleteTechnology(id);
